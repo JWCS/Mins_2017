b82074c4e017ca9f207336e8fa467f219e9abd3d
JWCS <sauve031@umn.edu>
2017-01-25 22:00:20 -0600

    Finished Archon, mysterious bugs however

--- Try2/Archon.java
+++ Try2/Archon.java
@@ -1,12 +1,6 @@
 package Try2;
 
-import battlecode.common.Clock;
-import battlecode.common.Direction;
-import battlecode.common.GameActionException;
-import battlecode.common.MapLocation;
-
-import static utils.Methods.randomDirection;
-import static utils.Methods.tryMove;
+import battlecode.common.*;
 
 /**
  * Package <Try2>, Mins_2017 Project
@@ -28,6 +22,11 @@ class Archon extends RobotCode {
             }
         }
         rc.broadcast(Constants.ARCHONS_ALIVE_CH, ++archonsHere);
+        // Check sparsity
+        for(int i = 0; i < 12; i++)
+            if(rc.senseNearbyTrees(here.add(Direction.getNorth().rotateLeftDegrees(30*i)), radius, Team.NEUTRAL).length > 0)
+                lumberjackBase += 0.1;
+        lumberjackBase += rc.senseNearbyTrees(-1, Team.NEUTRAL).length / 5 * 1.5;
     }
 
     int enemyArchonNo;
@@ -52,6 +51,7 @@ class Archon extends RobotCode {
     int cost                        = 0;
     float costTransfer              = 0.25f;
     int vpToBuy;
+    float budget = rc.getTeamBullets();
 
     void run() throws GameActionException {
         while (true) {
@@ -72,29 +72,37 @@ class Archon extends RobotCode {
                 // Update units in play
                 for(int i = 0; i < 6; i+=2)
                     haveUnits[i] = rc.readBroadcast(Constants.GARDENER_STABLE_CH + 2*i);
+                // Budget
+                budget = rc.getTeamBullets();
                 // Calc expected cost rn
                 cost = 50*(haveUnits[1]-haveUnits[0]) + ((haveUnits[1]-haveUnits[0] < gardenerRatio) ? 100 : 0); // Gardeners
                 cost += 100*(neededUnits[1] - haveUnits[2]);
                 cost += 80*(neededUnits[2] - haveUnits[3]);
                 cost += 100*(neededUnits[3] - haveUnits[4]);
                 cost += 300*(neededUnits[4] - haveUnits[5]);
-                vpToBuy = (int)( cost * costTransfer / rc.getVictoryPointCost);
+                //vpToBuy = (int)( cost * costTransfer / rc.getVictoryPointCost()); // Later?
+                vpToBuy = (int) (budget * 0.2 + (budget > 500 ? 0.5*(budget - 500) : 0));
+                rc.donate(vpToBuy*rc.getVictoryPointCost());
+
+                // Actual action
+                for(int i = 0; i < 36; ++i)
+                    if(haveUnits[1] - haveUnits[0] < 1 + gardenerRatio && rc.canHireGardener(Direction.getNorth().rotateLeftDegrees(10*i)))
+                        rc.hireGardener(Direction.getNorth().rotateLeftDegrees(10*i));
 
                 // Generate a random direction
-                Direction dir = randomDirection();
+                Direction dir = Direction.NORTH.rotateLeftDegrees((float)Math.random()*360);
 
                 // Randomly attempt to build a gardener in this direction
-                if (rc.canHireGardener(dir) && Math.random() < .01) {
-                    rc.hireGardener(dir);
-                }
+                if(rc.isBuildReady())
+                    for(int i = 0; i < 36; ++i)
+                        if(rc.canHireGardener(dir.rotateLeftDegrees(10*i))){
+                            rc.hireGardener(dir.rotateLeftDegrees(10*i));
+                            break;
+                        }
 
                 // Move randomly
-                tryMove(randomDirection());
-
-                // Broadcast archon's location for other robots on the team to know
-                MapLocation myLocation = rc.getLocation();
-                rc.broadcast(0,(int)myLocation.x);
-                rc.broadcast(1,(int)myLocation.y);
+                if(rc.canMove(dir))
+                    rc.move(dir);
 
                 // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
                 Clock.yield();
